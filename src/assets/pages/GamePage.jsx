4b89eb24d39b8eaf81b5b1c4b3ed9cb82d10ee1c
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-01-04 13:48:05 +0100

    add numbers

--- src/assets/pages/GamePage.jsx
+++ src/assets/pages/GamePage.jsx
@@ -1,17 +1,30 @@
 export default function GamePage() {
+  let numbers = [];
+  for (let i = 1; i <= 90; i++) {
+    numbers.push(i);
+  }
+  //console.log(numbers);
   return (
     <main className="main-content">
-      <div className="container d-flex">
-        <div className="row">
-          <div className="col-9">tombola</div>
-          <div className="col-3">
-            <div className="container d-flex flex-column  align-items-center">
-              <p>Ultimo numero estratto</p>
-              <div className="num-drawn"></div>
-              <button className="btn btn-secondary"> estrai numero </button>
-              <hr />
-              <button className="btn btn-secondary"> termina il gioco </button>
-            </div>
+      <div className="container d-flex ">
+        <div className="col-9">
+          <div className="container d-flex flex-wrap">
+            {numbers.map((num) => {
+              return (
+                <div key={num} className="singleNum">
+                  <span className="num">{num}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="container d-flex flex-column  align-items-center">
+            <p>Ultimo numero estratto</p>
+            <div className="num-drawn"></div>
+            <button className="btn btn-secondary"> estrai numero </button>
+            <hr />
+            <button className="btn btn-secondary"> termina il gioco </button>
           </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,4 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
