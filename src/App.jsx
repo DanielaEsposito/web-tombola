1f9199616f4a5bf810a95b925421757eef863de6
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-01-04 09:43:23 +0100

    add first structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+//pages
+import HomePage from "./assets/pages/HomePage";
+import GamePage from "./assets/pages/GamePage";
+//layouts
+import HomeLayout from "./assets/layouts/HomeLayout";
+import GameLayout from "./assets/layouts/GameLayout";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route Component={HomeLayout}>
+          <Route index Component={HomePage} />
+        </Route>
+        <Route Component={GameLayout}>
+          <Route path="/game" Component={GamePage} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
